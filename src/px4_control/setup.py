5e6bd31deac9e4ec56e9a796048fc3cef7a5f7ca
Kiwi <lequyphuong1903@gmail.com>
2026-09-26 10:42:57 -0400

    Refactor setup.py to use find_packages and improve package metadata

--- src/px4_control/setup.py
+++ src/px4_control/setup.py
@@ -1,19 +1,31 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'px4_control'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='kiwi',
+    maintainer_email='lehuo10@rowan.edu',
+    description='Control package for PX4',
+    license='ROWAN',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
-            'takeoff_land = px4_control.takeoff_land:main',
-            'arm_node = px4_control.arm_node:main',
+            'offboard_arm = px4_control.offboard_arm:main',
+            'takeoff_land_classic = px4_control.takeoff_land_classic:main',
             'takeoff_land_mission = px4_control.takeoff_land_mission:main',
         ],
     },
